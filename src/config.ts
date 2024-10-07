cd38e53cc294e5d12b7588ba4f402e8a0701456c
Markus Haack <markus.haack@gmail.com>
2024-10-07 22:17:49 +0200

    fix: icons

--- src/config.ts
+++ src/config.ts
@@ -14,8 +14,8 @@ export const siteConfig: SiteConfig = {
     fixed: false,     // Hide the theme color picker for visitors
   },
   banner: {
-    enable: false,
-    src: 'assets/images/demo-banner.png',   // Relative to the /src directory. Relative to the /public directory if it starts with '/'
+    enable: true,
+    src: 'assets/images/hero-banner.jpg',   // Relative to the /src directory. Relative to the /public directory if it starts with '/'
     position: 'center',      // Equivalent to object-position, only supports 'top', 'center', 'bottom'. 'center' by default
     credit: {
       enable: false,         // Display the credit text of the banner image
