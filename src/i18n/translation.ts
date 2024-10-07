4917290275b4252925fe31764856c08cc02e2fd3
Markus Haack <markus.haack@gmail.com>
2024-10-07 10:54:56 +0200

    chore: clean up

--- src/i18n/translation.ts
+++ src/i18n/translation.ts
@@ -2,11 +2,6 @@ import { siteConfig } from '../config'
 import type I18nKey from './i18nKey'
 import { de } from './languages/de'
 import { en } from './languages/en'
-import { es } from './languages/es'
-import { ja } from './languages/ja'
-import { ko } from './languages/ko'
-import { zh_CN } from './languages/zh_CN'
-import { zh_TW } from './languages/zh_TW'
 
 export type Translation = {
   [K in I18nKey]: string
@@ -17,17 +12,10 @@ const defaultTranslation = en
 const map: { [key: string]: Translation } = {
   de: de,
   de_de: de,
-  es: es,
   en: en,
   en_us: en,
   en_gb: en,
   en_au: en,
-  zh_cn: zh_CN,
-  zh_tw: zh_TW,
-  ja: ja,
-  ja_jp: ja,
-  ko: ko,
-  ko_kr: ko,
 }
 
 export function getTranslation(lang: string): Translation {
