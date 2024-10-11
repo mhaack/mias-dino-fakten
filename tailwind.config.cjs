81c95647d2ad14df188f8f27f477ed9a9b6cd37a
Markus Haack <markus.haack@gmail.com>
2024-10-11 13:10:11 +0200

    chore: fix hero image

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,14 +1,18 @@
 /** @type {import('tailwindcss').Config} */
-const defaultTheme = require("tailwindcss/defaultTheme")
+const defaultTheme = require('tailwindcss/defaultTheme')
 module.exports = {
-  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue,mjs}"],
-  darkMode: "class", // allows toggling dark mode manually
+  content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue,mjs}'],
+  darkMode: 'class', // allows toggling dark mode manually
   theme: {
     extend: {
       fontFamily: {
-        sans: ["Roboto", "sans-serif", ...defaultTheme.fontFamily.sans],
+        sans: ['Roboto', 'sans-serif', ...defaultTheme.fontFamily.sans],
+      },
+      objectPosition: {
+        'hero-banner-large': '50% 52%',
+        'hero-banner': '50% 20%',
       },
     },
   },
-  plugins: [require("@tailwindcss/typography")],
+  plugins: [require('@tailwindcss/typography')],
 }
